8012cccb578da3eaade1641af6d944ee58b5071d
Lindsey Maddox <dev@lindseymaddox.com>
2019-01-07 23:49:40 -0600

    panes scrolling, stopped short of getting an active state to work on the nav

--- _scripts/main.js
+++ _scripts/main.js
@@ -1,58 +1,10 @@
-var $showNav,
-	$nav;
-
-function initVars() {
-	$showNav 	= $('nav span#nav');
-	$nav 	= $('nav > ul');
+function setActive() {
+  aObj = document.getElementById('nav').getElementsByTagName('a');
+  for(i=0;i<aObj.length;i++) {
+    if(document.location.href.indexOf(aObj[i].href)>=0) {
+      aObj[i].className='active';
+    }
+  }
 }
 
-function showNav(){
-	$showNav.click(function(){
-		$nav.toggleClass('expanded');
-		return false;
-	});
-}
-
-function placeholderSupported() {
-    test = document.createElement('input');
-    return ('placeholder' in test);
-}
-
-function initPlaceholders() {
-	if ($('input[placeholder]').length > 0) {
-		if (!placeholderSupported()) {
-			$('input[placeholder]').focus(function() {
-				var input = $(this);
-				if (input.val() === input.attr('placeholder')) {
-					input.val('');
-					input.removeClass('placeholder');
-				}
-			}).blur(function() {
-				var input = $(this);
-				if (input.val() === '' || input.val() === input.attr('placeholder')) {
-					input.addClass('placeholder');
-					input.val(input.attr('placeholder'));
-				}
-			}).blur();
-			$('input[placeholder]').parents('form').submit(function() {
-				$(this).find('[placeholder]').each(function() {
-					var input = $(this);
-					if (input.val() === input.attr('placeholder')) {
-						input.val('');
-					}
-				});
-			});
-		}
-	}
-}
-
-function firstLoad() {
-	initVars();
-	showNav();
-	initPlaceholders();
-	placeholderSupported();
-}
-
-$(function() {
-	firstLoad();
-});
+window.onload = setActive;
