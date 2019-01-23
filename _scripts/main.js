cc821f491d83d5bea5af7df13c0a5031574cbe9e
Lindsey Maddox <dev@lindseymaddox.com>
2019-01-22 23:33:17 -0600

    employee bios and pins behaving properly using js loop

--- _scripts/main.js
+++ _scripts/main.js
@@ -1,20 +1,33 @@
 
 $(window).scroll(function() {
 
-    if ($(this).scrollTop()>500) {
-        $('.fade').fadeIn();
-    } else {
-      $('.fade').fadeOut();
-    }
+  if ($(this).scrollTop()>500) {
+      $('.fade').fadeIn();
+  } else {
+    $('.fade').fadeOut();
+  }
 
-    $(".greenup").click(function(){
-      var element = document.getElementById("greenup");
-      element.classList.add("show");
+  // Find all links within the staff container
+  $('.staff a').each(function(_index, link) {
+
+    // Loop through the list of links adding a click handler for each one
+    $(link).on('click', function() {
+
+      // Hide all bios to avoid showing more than one at a time
+      $('.bio').hide();
+
+      // Show the bio with the employee data attribute that matches the link clicked
+      $('.bio*[data-name=' + $(this).data( "name" ) + ']').show();
+
+      // Hide all name pins to avoid showing more than one at a time
+      $(".staff a").removeClass( "selected" );
+
+      // Add class to the clicked link
+      $(this).addClass("selected");
+
     });
 
-    $(".greenup").click(function(){
-      $(".greenup").addClass("selected");
-    });
+  });
 
 });
 
@@ -25,5 +38,5 @@ $(window).scroll(function() {
 // 1. Add a `data` attribute to identify each link/bio combo. Either an index like a number or maybe even the name of the person
 // 2. Find all the `.greenup` links on the page using jQuery, and add a click handler to them
 // 3. In that click handler I'd find the matching bio to display based on the data attribute attached to the link
-// 4. I'd call `.toggle()` on the bio to display and hide it once the link was clicked (edited)
+// 4. I'd call `.toggle()` on the bio to display and hide it once the link was clicked
 // * If you need to only show one at a time, which I suspect you do, I'd hide all bios when any link is clicked then toggle the one that matches the data attribute of the link clicked
--- _scripts/min/main-min.js
+++ _scripts/min/main-min.js
@@ -1,6 +1,18 @@
-$(window).scroll(function(){500<$(this).scrollTop()?$(".fade").fadeIn():$(".fade").fadeOut(),$(".greenup").click(function(){var e;document.getElementById("greenup").classList.add("show")}),$(".greenup").click(function(){$(".greenup").addClass("selected")})});
+$(window).scroll(function(){500<$(this).scrollTop()?$(".fade").fadeIn():$(".fade").fadeOut(),
+// Find all links within the staff container
+$(".staff a").each(function(a,e){
+// Loop through the list of links adding a click handler for each one
+$(e).on("click",function(){
+// Hide all bios to avoid showing more than one at a time
+$(".bio").hide(),
+// Show the bio with the employee data attribute that matches the link clicked
+$(".bio*[data-name="+$(this).data("name")+"]").show(),
+// Hide all name pins to avoid showing more than one at a time
+$(".staff a").removeClass("selected"),
+// Add class to the clicked link
+$(this).addClass("selected")})})});
 // 1. Add a `data` attribute to identify each link/bio combo. Either an index like a number or maybe even the name of the person
 // 2. Find all the `.greenup` links on the page using jQuery, and add a click handler to them
 // 3. In that click handler I'd find the matching bio to display based on the data attribute attached to the link
-// 4. I'd call `.toggle()` on the bio to display and hide it once the link was clicked (edited)
+// 4. I'd call `.toggle()` on the bio to display and hide it once the link was clicked
 // * If you need to only show one at a time, which I suspect you do, I'd hide all bios when any link is clicked then toggle the one that matches the data attribute of the link clicked
