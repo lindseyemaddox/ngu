ec7026eb1b0ebeb11cdeaefd4c5fe2a701862998
Lindsey Maddox <dev@lindseymaddox.com>
2020-06-07 16:32:18 -0500

    restructure for firebase include public directory

--- min/app-min.js
+++
@@ -1,7 +0,0 @@
-// Load http module.
-var http=require("http"),express=require("express"),app=new express;
-// Load express module.
-// Basic routing.
-app.get("/",function(e,r){r.send("Front page")}),
-// Create server and listen on port 3030.
-http.createServer(app).listen(3030,function(){console.log("Server running...")});
